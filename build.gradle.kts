d24faa453eb6e1bd1a8b09625b1f38381254a87b
Jan1du <janidu.vihangasara345@gmail.com>
2026-03-06 08:55:44 +0000

    Created pebble templates for searching page and register/login pages. Also implemented the routing configurations for the these pages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation("io.ktor:ktor-server-status-pages:${version}")
+    implementation("io.ktor:ktor-server-pebble:${version}")
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,11 +3,39 @@ package com
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.pebble.respondTemplate
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        get("/") { call.displaySearchForm() }
+        get("/manage") { call.displayManage()}
+        get("/about") { call.displayAbout() }
+        get("/register") { call.registerPage() }
+        get("/login") { call.loginPage() }
+        get("/search") { call.displayResults() }
     }
+}
+
+private suspend fun ApplicationCall.displaySearchForm() {
+    respondTemplate("search-form.peb", model = mapOf("active_nav" to "book"))
+}
+
+private suspend fun ApplicationCall.displayResults() {
+    respondTemplate("flight-results.peb", model = mapOf("active_nav" to "book"))
+}
+
+private suspend fun ApplicationCall.displayManage() {
+    respondTemplate("manage.peb", model = mapOf("active_nav" to "manage"))
+}
+
+private suspend fun ApplicationCall.displayAbout() {
+    respondTemplate("about-us.peb", model = mapOf("active_nav" to "about"))
+}
+
+private suspend fun ApplicationCall.registerPage() {
+    respondTemplate("register.peb", model = mapOf("active_nav" to "register"))
+}
+
+private suspend fun ApplicationCall.loginPage() {
+    respondTemplate("login.peb", model = mapOf("active_nav" to "login"))
 }
