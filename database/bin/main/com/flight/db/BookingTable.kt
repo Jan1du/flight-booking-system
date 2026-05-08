9cc4c02793ff20a15add0603e5deee92934a166f
Brian Wong <146480210+Princebw30@users.noreply.github.com>
2026-05-08 01:30:42 +0000

    Minor fixes

--- database/bin/main/com/flight/db/BookingTable.kt
+++ database/bin/main/com/flight/db/BookingTable.kt
@@ -3,14 +3,14 @@ package com.flight.db
 import org.jetbrains.exposed.v1.core.ReferenceOption
 import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 
-const val MAX_DATE_LENGTH = 10 // yyyy-mm-dd
-const val MAX_STATUS_LENGTH = 9 // completed, confirmed, pending 
+const val MAX_BOOKING_DATE_LENGTH = 10 // yyyy-mm-dd
+const val MAX_BOOKING_STATUS_LENGTH = 9 // completed, confirmed, pending 
 
-object BookingTable : IntIdTable("bookings") {
+object BookingTable : IntIdTable("booking") {
     // Foreign keys
     val user = reference("user_id", UserTable, ReferenceOption.CASCADE)
 
-    val bookingDate = varchar("booking_date", MAX_DATE_LENGTH)
+    val bookingDate = varchar("booking_date", MAX_BOOKING_DATE_LENGTH)
     val paxCount = integer("pax_count")
-    val status = varchar("status", MAX_STATUS_LENGTH)
+    val status = varchar("status", MAX_BOOKING_STATUS_LENGTH)
 }
